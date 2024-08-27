140c2da903d4e1fe3958524c3882ae3be6a76860
John Paul <113358424+Viadsss@users.noreply.github.com>
2024-08-27 20:03:00 +0800

    Style navigation

--- public/js/navigation.js
+++ public/js/navigation.js
@@ -1,4 +1,9 @@
+const menuButton = document.getElementById("menuButton");
+
+menuButton.addEventListener("click", toggleMenu);
+
 function toggleMenu() {
   const navMenu = document.getElementById("navMenu");
   navMenu.classList.toggle("show");
+  menuButton.classList.toggle("change");
 }
